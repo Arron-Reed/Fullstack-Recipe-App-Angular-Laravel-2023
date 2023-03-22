0d1400e85a4fff400d6ce3c560ca40a4f76b27f3
Arron-Reed <arron.reed@chasacademy.se>
2023-03-22 21:47:26 +0100

    Commit before API connection

---
+++ Angular-FrontEnd/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { RecipeCardComponent } from './recipe-card/recipe-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RecipeCardComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
