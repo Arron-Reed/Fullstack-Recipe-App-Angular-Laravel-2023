81a06566a039f6333170c7aac32b9ae81e849e13
Arron-Reed <arron.reed@chasacademy.se>
2023-03-22 22:41:48 +0100

    Updates

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeCardComponent } from './recipe-card/recipe-card.component';
 
@@ -10,7 +11,8 @@ import { RecipeCardComponent } from './recipe-card/recipe-card.component';
     RecipeCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
